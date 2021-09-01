9ab37c5556a06fda5a388b09e7ea50843989432a
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-01 14:23:39 -0300

    resolução de exercicios

---
+++ src/br/com/zup/Lista06Ex01.java
@@ -0,0 +1,9 @@
+package br.com.zup;
+
+public class Lista06Ex01 {
+    public static void main(String[] args) {
+        /* LISTA DE EXERCÍCIOS - REVISÃO MÓDULO 2
+        1 - Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar, sabendo que a decisão é sempre pelo mais barato. */
+
+    }
+}
---
+++ src/br/com/zup/Lista06Ex09.java
@@ -0,0 +1,25 @@
+package br.com.zup;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Lista06Ex09 {
+    public static void main(String[] args) {
+        // 9 - Faça um Programa que leia uma lista de 10 números e mostre-os na ordem inversa.
+        Scanner leitor = new Scanner(System.in);
+
+        List<Integer> numeros = new ArrayList<>();
+        int contador = 1;
+        int totalDeNumeros = 10;
+        int valorDigitadoPeloUsuario = 0;
+
+        while (contador <= totalDeNumeros) {
+        System.out.println("Digite o  " + contador + "º número:");
+        valorDigitadoPeloUsuario = leitor.nextInt();
+        numeros.add(valorDigitadoPeloUsuario);
+        contador++
+        }
+        System.out.println(numeros);
+    }
+}

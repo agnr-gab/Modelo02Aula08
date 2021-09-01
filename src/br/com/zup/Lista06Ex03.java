efe0b3708add366e3d7a8db059de183249f62784
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-01 17:25:47 -0300

    exercicio 03 tabuada usando for e if

---
+++ src/br/com/zup/Lista06Ex03.java
@@ -0,0 +1,26 @@
+package br.com.zup;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class Lista06Ex03 {
+    public static void main(String[] args) {
+
+    /* LISTA DE EXERCÍCIOS - REVISÃO MÓDULO 2
+    3 - Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10. O usuário deve informar de qual número ele deseja ver a tabuada. A saída deve ser conforme o exemplo abaixo:*/
+        Scanner leitorTabuada = new Scanner(System.in);
+
+        int numeroTabuada; //entrada do usuario
+        System.out.println("Para verificar a tabuada digite um número entre 1 e 10:");
+        numeroTabuada = leitorTabuada.nextInt();
+        if (numeroTabuada <= 0 || numeroTabuada > 10) {
+            System.out.println("Insira um número válido entre 1 e 10");
+            numeroTabuada = leitorTabuada.nextInt();
+        for (int N = 1; N <= 10; N++) {
+            System.out.println(numeroTabuada + " x " + N + " = " + numeroTabuada * N);
+
+            }
+        }
+    }
+}

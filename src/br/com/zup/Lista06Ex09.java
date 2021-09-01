8c8584e856d2789473a067a50435bf0451e148f9
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-01 14:33:01 -0300

    exercicio 09

--- src/br/com/zup/Lista06Ex09.java
+++ src/br/com/zup/Lista06Ex09.java
@@ -11,15 +11,20 @@ public class Lista06Ex09 {
 
         List<Integer> numeros = new ArrayList<>();
         int contador = 1;
+        int contadorDeExibicao = 9;
+        int finalDeExibicao = 0;
         int totalDeNumeros = 10;
         int valorDigitadoPeloUsuario = 0;
 
         while (contador <= totalDeNumeros) {
-        System.out.println("Digite o  " + contador + "º número:");
-        valorDigitadoPeloUsuario = leitor.nextInt();
-        numeros.add(valorDigitadoPeloUsuario);
-        contador++
+            System.out.println("Digite o " + contador + "º número:");
+            valorDigitadoPeloUsuario = leitor.nextInt();
+            numeros.add(valorDigitadoPeloUsuario);
+            contador++;
         }
-        System.out.println(numeros);
+        while (contadorDeExibicao >= finalDeExibicao) {
+            System.out.println(numeros.get(contadorDeExibicao));
+            contadorDeExibicao --;
+        }
     }
 }

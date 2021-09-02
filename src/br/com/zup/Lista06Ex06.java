107ed00a90f45353cc27bafdcebea7fca03e61ff
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-01 23:34:36 -0300

    exercicio 06

---
+++ src/br/com/zup/Lista06Ex06.java
@@ -0,0 +1,14 @@
+package br.com.zup;
+
+public class Lista06Ex06 {
+    public static void main(String[] args) {
+        // 6 - Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
+        int impar;
+        int contador;
+
+
+
+
+
+    }
+}
